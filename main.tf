d49a4bd7a5c138b3ec1bb5e9a5ccd08fb622db41
Adeshola Shokunbi <sulazo@yahoo.com>
2025-07-05 19:03:47 -0400

    changed main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@
 terraform {
   required_providers {
     local = {
-      source  = "hashicorp/local"   # The provider plugin source
-      version = "~> 2.0"            # Use version 2.x of the local provider
+      source  = "hashicorp/local" # The provider plugin source
+      version = "~> 2.0"          # Use version 2.x of the local provider
     }
   }
 }
@@ -20,6 +20,6 @@ provider "local" {}
 # Create a simple file on the local machine
 # ---------------------------------------------
 resource "local_file" "hello_world" {
-  filename = "hello.txt"                           # Name of the file to create
-  content  = "Hello, world from Terraform!!"        # Text to write inside the file
+  filename = "hello.txt"                     # Name of the file to create
+  content  = "Hello, world from Terraform!!" # Text to write inside the file
 }
